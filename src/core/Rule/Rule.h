f614dbf5c366194c0ae793eec164864f55251c36
Vecta6 <bleuleloup066@gmail.com>
2025-12-05 11:18:35 +0100

    fix: resolve merge conflict in Rule.h to restore build

--- src/core/Rule/Rule.h
+++ src/core/Rule/Rule.h
@@ -6,15 +6,8 @@
 #ifndef CESI_PROJECT_POO_25_26_RULE_H
 #define CESI_PROJECT_POO_25_26_RULE_H
 
-<<<<<<< HEAD
-
-
-#include "../Cells/CellState.h"
-#include "../Cells/Cell.h"
-=======
 class Cell;
 class CellState;
->>>>>>> be0fac0889aeafe0555e5fba1ae29c1a654ac777
 
 class Rule {
 public:
